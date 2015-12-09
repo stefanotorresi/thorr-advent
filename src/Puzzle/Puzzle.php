6ef346e891001cad683a7410027469d892dbb32c
Stefano Torresi <stefano@torresi.io>
2015-12-09 19:28:07 +0100

    fix copyright notice

--- src/Puzzle/Puzzle.php
+++ src/Puzzle/Puzzle.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author  Stefano Torresi (http://stefanotorresi.it)
- * @license See the file LICENSE.txt for copying permission.
+ * @license See the file LICENSE for copying permission.
  * ************************************************
  */
 
